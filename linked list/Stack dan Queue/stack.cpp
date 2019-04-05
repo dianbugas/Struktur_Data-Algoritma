2c9e419232a558d9dcda81786a1f305c10fb17b4
dianynf <dianbugas@gmail.com>
2019-04-05 22:09:59 +0700

    stack

---
+++ linked list/Stack dan Queue/stack.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_STACK 5
+
+//DEKLARASI
+struct node{
+    int data;
+    node *next;
+};
+node *push, *pop = NULL, *bantu = NULL, *entry, *temp;
+
+void push(){}
