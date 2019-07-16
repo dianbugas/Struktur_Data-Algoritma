e80052e1143db32137916a411f9a9a8fc363c0a0
dianynf <dianbugas@gmail.com>
2019-07-16 17:32:34 +0700

    rekursif

--- rekursif/reqursif.cpp
+++ rekursif/reqursif.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 using namespace std;
 
-long faktorial (int f){
-    if(f==0)
+//fungsi dari rekursif
+long rekursiffaktorial(int f,int n)
+{
+    if (f == 0)
         return 1;
     else
-        return f * faktorial(f-1);
+        return n * rekursiffaktorial(f - 1,n);
 }
 
 int main()
 {
-    int x;
-    cout<<"input faktorial";
-    cin>>x;
-    cout<<"faktorial dari"<<x<<"adalah"<<faktorial(x)<<endl;
-    return 0;
+    int n,f;
+    cout<<"masukkan nilai : ";
+    cin>>f;
+    cout<<"masukan pangkat : ";
+    cin>>n;
+    cout <<"nilai "<< f << " pangkat "<<n<<" = "<< rekursiffaktorial(n,f) << endl;
 }
